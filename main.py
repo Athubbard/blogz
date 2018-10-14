49d6f1bab192c91070685d837862800117e66ae3
Amber Hubbard <amberthubbard@gmail.com>
2018-10-14 12:38:45 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, redirect, render_template,session
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:buildablog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(250))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+@app.route('/newpost', methods=['GET', 'POST'])
+def post():
+    if request.method == 'POST':
+        blog_name = request.form['title']
+        blog_body = request.form['body']
+        new_blog = Blog(blog_name, blog_body) 
+        db.session.add(new_blog)
+        db.session.commit()
+
+    return render_template('newpost.html')
+#    blog = Blog.query.all()
+app.run()
